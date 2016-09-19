eb66bdb7daf635a7b000b6280033000c0f2ba5ff
SergioBertolinSG <sbertolin@solidgear.es>
2016-09-19 12:13:17 +0200

    Adapted playbook and install script to makefile

--- install_master_owncloud.sh
+++ install_master_owncloud.sh
@@ -1,7 +1,11 @@
 sudo rm -rf core
-git clone https://github.com/owncloud/core.git
+git clone --depth 1 https://github.com/owncloud/core.git
 cd core
-git submodule update --init --recursive
+git fetch --unshallow
+git pull --all
+git remote set-branches origin '*'
+git fetch -vvv
+make
 cd ..
 fecha=`date +%d-%m-%y`
 sudo cp -r core "/var/www/html/master_$fecha"
