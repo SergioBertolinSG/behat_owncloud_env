5ced2813c76f0ce6cf8e20838831968a23a01431
Sergio Bertolín <sbertolin@solidgear.es>
2017-03-06 14:08:57 +0100

    Change owncloud script to run make as www-data user

--- install_master_owncloud.sh
+++ install_master_owncloud.sh
@@ -1,14 +1,9 @@
 sudo rm -rf core
-#git clone --depth 1 https://github.com/owncloud/core.git
 git clone https://github.com/owncloud/core.git
 cd core
-#git fetch --unshallow
-#git pull --all
-#git remote set-branches origin '*'
-#git fetch -vvv
-make
-cd ..
 fecha=`date +%d-%m-%y`
 sudo cp -r core "/var/www/html/master_$fecha"
 sudo chown -R www-data:www-data "/var/www/html/master_$fecha"
+cd "/var/www/html/master_$fecha"
+sudo -u www-data make
 
