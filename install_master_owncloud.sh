c4725a0035775f626c19437101237c6d325f546c
SergioBertolinSG <sbertolin@solidgear.es>
2017-03-07 11:04:08 +0100

    ubuntu is the apache user here

--- install_master_owncloud.sh
+++ install_master_owncloud.sh
@@ -1,9 +1,8 @@
 sudo rm -rf core
 git clone https://github.com/owncloud/core.git
-cd core
 fecha=`date +%d-%m-%y`
 sudo cp -r core "/var/www/html/master_$fecha"
-sudo chown -R www-data:www-data "/var/www/html/master_$fecha"
+sudo chown -R ubuntu:www-data "/var/www/html/master_$fecha"
 cd "/var/www/html/master_$fecha"
-sudo -u www-data make
+make
 
--- reboot_owncloud_installation.sh
+++ reboot_owncloud_installation.sh
@@ -15,7 +15,7 @@ mysql -u root -h localhost -e "drop database owncloud"
 mysql -u root -h localhost -e "drop user oc_admin"
 mysql -u root -h localhost -e "drop user oc_admin@localhost"
 
-sudo -u www-data $installation_path/occ maintenance:install --database='mysql' --database-name='owncloud' --database-user='root' --database-pass='' --admin-user='admin' --admin-pass='admin'
+$installation_path/occ maintenance:install --database='mysql' --database-name='owncloud' --database-user='root' --database-pass='' --admin-user='admin' --admin-pass='admin'
 
 IP_ADDRESS=$(ifconfig eth1 2>/dev/null|awk '/inet addr:/ {print $2}'|sed 's/addr://')
-sudo -u www-data $installation_path/occ config:system:set trusted_domains 2 --value=$IP_ADDRESS
+$installation_path/occ config:system:set trusted_domains 2 --value=$IP_ADDRESS
